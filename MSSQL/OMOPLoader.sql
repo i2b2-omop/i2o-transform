056e597e386ffcd69b3580d7c57eec115e41f1ea
Matthew Joss <mjoss@partners.org>
2017-01-13 16:08:17 -0500

    fixed qoute numbers for %source_value field inserts in demographics

--- MSSQL/OMOPLoader.sql
+++ MSSQL/OMOPLoader.sql
@@ -218,7 +218,7 @@ DECLARE @batchid numeric
 declare getsql cursor local for 
 --1 --  S,R,NH
 	select 'insert into person(gender_source_value,race_source_value,ethnicity_source_value,person_id,year_of_birth,month_of_birth,day_of_birth,time_of_birth,gender_concept_id,ethnicity_concept_id,race_concept_id) '+ --person(raw_sex,PATID, BIRTH_DATE, BIRTH_TIME,SEX, HISPANIC, RACE) 
-	'	select p.sex_cd+'':'' '''+sex.c_name+''',p.race_cd+'':'' '''+race.c_name+''',p.race_cd,patient_num, '+
+	'	select p.sex_cd+'':'+sex.c_name+''',p.race_cd+'':'+race.c_name+''',p.race_cd,patient_num, '+
 	'	year(birth_date), '+
     '	month(birth_date), '+
     '	day(birth_date), '+
@@ -237,7 +237,7 @@ declare getsql cursor local for
 	and sex.c_visualattributes like 'L%'
 union -- A - S,R,H
 select 'insert into person(gender_source_value,race_source_value,ethnicity_source_value,person_id,year_of_birth,month_of_birth,day_of_birth,time_of_birth,gender_concept_id,ethnicity_concept_id,race_concept_id) '+
-	'	select p.sex_cd+'':'' '''+sex.c_name+''',p.race_cd+'':'' '''+race.c_name+''',p.race_cd+'':'' '''+hisp.c_name+''',patient_num, '+ --'	select p.sex_cd+'':''+sex.c_name,p.race_cd+'':''+race.c_name,p.race_cd+'':''+hisp.c_name,patient_num, '+
+	'	select p.sex_cd+'':'+sex.c_name+''',p.race_cd+'':'+race.c_name+''',p.race_cd+'':'+hisp.c_name+''',patient_num, '+ --'	select p.sex_cd+'':''+sex.c_name,p.race_cd+'':''+race.c_name,p.race_cd+'':''+hisp.c_name,patient_num, '+
 	'	year(birth_date), '+
     '	month(birth_date), '+
     '	day(birth_date), '+
@@ -259,7 +259,7 @@ select 'insert into person(gender_source_value,race_source_value,ethnicity_sourc
 	and sex.c_visualattributes like 'L%'
 union --2 S, nR, nH
 	select 'insert into person(gender_source_value,race_source_value,ethnicity_source_value,person_id,year_of_birth,month_of_birth,day_of_birth,time_of_birth,gender_concept_id,ethnicity_concept_id,race_concept_id) '+
-	'	select p.sex_cd+'':'' '''+sex.c_name+''',p.race_cd,p.race_cd,patient_num, '+ --'	select p.sex_cd,p.race_cd,p.race_cd,patient_num, '+
+	'	select p.sex_cd+'':'+sex.c_name+''',p.race_cd,p.race_cd,patient_num, '+ --'	select p.sex_cd,p.race_cd,p.race_cd,patient_num, '+
 	'	year(birth_date), '+
     '	month(birth_date), '+
     '	day(birth_date), '+
@@ -276,7 +276,7 @@ union --2 S, nR, nH
 	and sex.c_visualattributes like 'L%'
 union --3 -- nS,R, NH
 	select 'insert into person(gender_source_value,race_source_value,ethnicity_source_value,person_id,year_of_birth,month_of_birth,day_of_birth,time_of_birth,gender_concept_id,ethnicity_concept_id,race_concept_id) '+
-	'	select p.sex_cd,p.race_cd+'':'' '''+race.c_name+''',p.race_cd,patient_num, '+
+	'	select p.sex_cd,p.race_cd+'':'+race.c_name+''',p.race_cd,patient_num, '+
 	'	year(birth_date), '+
     '	month(birth_date), '+
     '	day(birth_date), '+
@@ -293,7 +293,7 @@ union --3 -- nS,R, NH
 	and race.c_visualattributes like 'L%'
 union --B -- nS,R, H
 	select 'insert into person(gender_source_value,race_source_value,ethnicity_source_value,person_id,year_of_birth,month_of_birth,day_of_birth,time_of_birth,gender_concept_id,ethnicity_concept_id,race_concept_id) '+
-	'	select p.sex_cd,p.race_cd+'':'' '''+race.c_name+''',p.race_cd+'':'' '''+hisp.c_name+''',patient_num, '+
+	'	select p.sex_cd,p.race_cd+'':'+race.c_name+''',p.race_cd+'':'+hisp.c_name+''',patient_num, '+
 	'	year(birth_date), '+
     '	month(birth_date), '+
     '	day(birth_date), '+
@@ -313,7 +313,7 @@ union --B -- nS,R, H
 	and hisp.c_visualattributes like 'L%'
 union --4 -- S, NR, H
 	select 'insert into person(gender_source_value,race_source_value,ethnicity_source_value,person_id,year_of_birth,month_of_birth,day_of_birth,time_of_birth,gender_concept_id,ethnicity_concept_id,race_concept_id) '+
-	'	select p.sex_cd+'':'' '''+sex.c_name+''',p.race_cd,p.race_cd,patient_num, '+
+	'	select p.sex_cd+'':'+sex.c_name+''',p.race_cd,p.race_cd,patient_num, '+
 	'	year(birth_date), '+
     '	month(birth_date), '+
     '	day(birth_date), '+
