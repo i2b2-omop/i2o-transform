ad0004270755388f93e0259f6187b0425ca23dc3
Kevin Embree <kembree1@partners.org>
2020-12-07 17:49:00 -0500

    Manually added 12 of the most common unit mappings

--- MSSQL/preparePHSOntology.sql
+++ MSSQL/preparePHSOntology.sql
@@ -73,3 +73,112 @@ create view i2o_ontology_lab as (select * from AllOfUs_Mart..i2b2metadata where 
 GO
 create view i2o_ontology_drug as (select * from AllOfUs_Mart..pcornet_med where i_stddomain='RxNorm' or i_stddomain='NDC')
 GO
+
+--Manually UPSERT unit mappings for most commonly used unit values in the PHS XML values
+IF EXISTS ( SELECT * FROM i2o_unitsmap u where u.units_name = 'mg/dl' )
+    UPDATE dbo.i2o_unitsmap
+        SET concept_id = 8840,
+		standard_concept = 'S'
+    WHERE units_name = 'mg/dl';
+ELSE 
+    INSERT dbo.i2o_unitsmap ( units_name, concept_id, standard_concept )
+    VALUES ( 'mg/dl', 8840, 'S' );
+
+IF EXISTS ( SELECT * FROM i2o_unitsmap u where u.units_name = '%')
+    UPDATE dbo.i2o_unitsmap
+        SET concept_id = 8554,
+		standard_concept = 'S'
+    WHERE units_name = '%';
+ELSE 
+    INSERT dbo.i2o_unitsmap ( units_name, concept_id, standard_concept )
+    VALUES ( '%', 8554, 'S' );
+
+IF EXISTS ( SELECT * FROM i2o_unitsmap u where u.units_name = 'mmol/l' )
+    UPDATE dbo.i2o_unitsmap
+        SET concept_id = 8753,
+		standard_concept = 'S'
+    WHERE units_name = 'mmol/l';
+ELSE 
+    INSERT dbo.i2o_unitsmap ( units_name, concept_id, standard_concept )
+    VALUES ( 'mmol/l', 8753, 'S' );
+
+IF EXISTS ( SELECT * FROM i2o_unitsmap u where u.units_name = 'g/dl' )
+    UPDATE dbo.i2o_unitsmap
+        SET concept_id = 8950,
+		standard_concept = 'S'
+    WHERE units_name = 'g/dl';
+ELSE 
+    INSERT dbo.i2o_unitsmap ( units_name, concept_id, standard_concept )
+    VALUES ( 'g/dl', 8950, 'S' );
+
+IF EXISTS ( SELECT * FROM i2o_unitsmap u where u.units_name = 'k/ul' )
+    UPDATE dbo.i2o_unitsmap
+        SET concept_id = 8792,
+		standard_concept = 'S'
+    WHERE units_name = 'k/ul';
+ELSE 
+    INSERT dbo.i2o_unitsmap ( units_name, concept_id, standard_concept )
+    VALUES ( 'k/ul', 8792, 'S' );
+
+IF EXISTS ( SELECT * FROM i2o_unitsmap u where u.units_name = 'u/l' )
+    UPDATE dbo.i2o_unitsmap
+        SET concept_id = 8645,
+		standard_concept = 'S'
+    WHERE units_name = 'u/l';
+ELSE 
+    INSERT dbo.i2o_unitsmap ( units_name, concept_id, standard_concept )
+    VALUES ( 'u/l', 8645, 'S' );
+
+IF EXISTS ( SELECT * FROM i2o_unitsmap u where u.units_name = 'th/cmm' )
+    UPDATE dbo.i2o_unitsmap
+        SET concept_id = 8961,
+		standard_concept = 'S'
+    WHERE units_name = 'th/cmm';
+ELSE 
+    INSERT dbo.i2o_unitsmap ( units_name, concept_id, standard_concept )
+    VALUES ( 'th/cmm', 8961, 'S' );
+
+IF EXISTS ( SELECT * FROM i2o_unitsmap u where u.units_name = 'fl' )
+    UPDATE dbo.i2o_unitsmap
+        SET concept_id = 8583,
+		standard_concept = 'S'
+    WHERE units_name = 'fl';
+ELSE 
+    INSERT dbo.i2o_unitsmap ( units_name, concept_id, standard_concept )
+    VALUES ( 'fl', 8583, 'S' );
+
+IF EXISTS ( SELECT * FROM i2o_unitsmap u where u.units_name = 'ml/min/1.73m2' )
+    UPDATE dbo.i2o_unitsmap
+        SET concept_id = 9117,
+		standard_concept = 'S'
+    WHERE units_name = 'ml/min/1.73m2';
+ELSE 
+    INSERT dbo.i2o_unitsmap ( units_name, concept_id, standard_concept )
+    VALUES ( 'ml/min/1.73m2', 9117, 'S' );
+
+IF EXISTS ( SELECT * FROM i2o_unitsmap u where u.units_name = 'gm/dl' )
+    UPDATE dbo.i2o_unitsmap
+        SET concept_id = 8713,
+		standard_concept = 'S'
+    WHERE units_name = 'gm/dl';
+ELSE 
+    INSERT dbo.i2o_unitsmap ( units_name, concept_id, standard_concept )
+    VALUES ( 'gm/dl', 8713, 'S' );
+
+IF EXISTS ( SELECT * FROM i2o_unitsmap u where u.units_name = 'th/cumm' )
+    UPDATE dbo.i2o_unitsmap
+        SET concept_id = 8961,
+		standard_concept = 'S'
+    WHERE units_name = 'th/cumm';
+ELSE 
+    INSERT dbo.i2o_unitsmap ( units_name, concept_id, standard_concept )
+    VALUES ( 'th/cumm', 8961, 'S' );
+
+IF EXISTS ( SELECT * FROM i2o_unitsmap u where u.units_name = 'pg/rbc' )
+    UPDATE dbo.i2o_unitsmap
+        SET concept_id = 8704,
+		standard_concept = 'S'
+    WHERE units_name = 'pg/rbc';
+ELSE 
+    INSERT dbo.i2o_unitsmap ( units_name, concept_id, standard_concept )
+    VALUES ( 'pg/rbc', 8704, 'S' );
